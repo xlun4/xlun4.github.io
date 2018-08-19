bf1a472538df9f17a62717d015f207bdf053083a
Nightly <nightly@outlook.kr>
2018-08-19 17:27:35 +0900

    .

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+var res_empty=new Response("")
+,	url_core={
+	"/notify":function(){
+		var j=this.json();
+		registration.showNotification(j[0],j[1]);
+		return res_empty;
+	}
+},	url_list=[url_core];
 oninstall=function(e){
 
 };
@@ -6,5 +14,12 @@ onactivate=function(e){
 };
 onfetch=function(e){
 	console.log(e.request);
+	p_url(e.request.url.replace(/^[^\/]+\/\/[^\/]+/,""));
+
+	for(var i=url_list.length-1;i>=0;i--){
+		if(!url_list[i][path])continue;
+		e.respondWith(fetch(url_list[i][path].call(e.request)));
+		return;
+	}
 	e.respondWith(fetch(e.request));
 };
